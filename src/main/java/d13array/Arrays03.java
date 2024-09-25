70e1a531fb2b4eb8dd6ffd93c41d3691fd96680b
EkremTnckr <ekremtunckir@gmail.com>
2024-09-25 22:12:45 +0300

    arrays,multidimensionsarray

---
+++ src/main/java/d13array/Arrays03.java
@@ -0,0 +1,84 @@
+package d13array;
+
+import java.util.Arrays;
+
+public class Arrays03 {
+    public static void main(String[] args) {
+
+        //Ornek 1: Bir int array olusturun ve 5 ten kucuk olan elemanlari yazdirin.
+        //Example 1: Create an int array and write the elements less than 5.
+        //      [12, 3, -3, 5, 23] ==> 3, -3
+
+        int[] numbers = {12, 3, -3, 5, 23};//kisa yol ile array olusturduk.
+        System.out.println(Arrays.toString(numbers));
+
+        //[12,3,-3,5,23]
+        for (int w : numbers) {
+            if (w < 5) {
+                System.out.print(w + " , ");//3 ,-3
+
+            }
+
+        }
+
+        //-----------------------------------------
+        //Ornek 2: Spesifik bir elemanin Array’de olup olmadigini anlamak icin gereken kodu yaziniz.
+        //Example 2: Write the code to find out if a specific element is in Array
+        //binarySearch() methodu var olan elemanlar icin size o elemanin index’ini verir
+        //binarySearch() methodunu sort() kullanmadan kullanmayiniz
+        //binarySearch() methodu olmayan elemanlar icin size negatif tamsayi verir
+        // “-” ==> isaretinin anlami eleman yok demektir
+
+    /*
+Yani, metot şu şekilde çalışır:
+    Diziyi ikiye bölün.
+    Aranan değer, ortadaki elemana eşitse, arama tamamlanır.
+    Aranan değer, ortadaki elemandan küçükse, dizinin sol yarısında aramaya devam edilir.
+    Aranan değer, ortadaki elemandan büyükse, dizinin sağ yarısında aramaya devam edilir.
+    Bu işlem, aranan değer bulunana veya dizi içinde aranacak bir alt dizi kalmayana kadar tekrar edilir.
+ */
+        System.out.println("--------------------------------");
+        String names[] = {"A", "T", "K", "E", "B"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+
+        int result = Arrays.binarySearch(names, "B");
+        System.out.println(result);
+
+        int result1 = Arrays.binarySearch(names, "C");
+        System.out.println(result1);
+
+        //-----------------------------------
+
+        int[] arr = {2, 1, 7, 6};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));//[1,2,6,7]
+        System.out.println(Arrays.binarySearch(arr, 2));//
+        System.out.println(Arrays.binarySearch(arr, 7));//
+        System.out.println(Arrays.binarySearch(arr, 9));//
+
+        String[] s = {"A", "C", "B", "D"};
+        Arrays.sort(s);
+        System.out.println(Arrays.toString(s));
+
+        System.out.println(Arrays.binarySearch(s, "A"));
+        System.out.println(Arrays.binarySearch(s, "C"));
+        System.out.println(Arrays.binarySearch(s, "E"));
+        System.out.println(Arrays.binarySearch(s, "G"));
+
+        //------------------------------------------------------------
+        //Ornek 3: Size verilen bir cumlede kac kelime oldugunu gosteren kodu yaziniz.
+        //Example 3: Write the code that shows you how many words are in a given sentence.
+        //String s = “Java is easy. Learn Java earn money.“;
+
+        String s1 = "Java is easy. Learn Java earn money.";
+        System.out.println(s1); //Java is easy. Learn Java earn money.
+
+        String[] words = s1.split(" ");
+
+        System.out.println(Arrays.toString(words)); //[Java, is, easy., Learn, Java, earn, money.]
+
+        System.out.println(words.length); //7
+
+    }
+}
